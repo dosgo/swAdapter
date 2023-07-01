11fe993da386b26c5ba9ce377f5f7e990ef0ab20
dosgo <dosgo@qq.com>
2023-07-02 02:00:19 +0800

    Update swServer.php

--- swServer.php
+++ swServer.php
@@ -1,9 +1,12 @@
 <?php
+ini_set('opcache.enable',0);
+ini_set('opcache.enable_cli',0);
 define("SWROOT",str_replace(basename(__FILE__),'',__FILE__).'../../');
-/* crontab
-* * * * * /usr/local/php/bing/php swServer.php
+/*
+*注意禁止开启协程功能,如果开启不得使用超全局变量,全局变量,静态类,单例数据库模式
+*如果开启协程超全局变量需要使用协程上下文,数据库 redis等要用连接池
 */
-if (version_compare(phpversion('swoole'), '4.6.2', '<')) {
+if (version_compare(phpversion('swoole'), '4.5.0', '<')) {
     echo 'Requires version 4.6.2 or higher'."\r\n";
     exit(0);
 }
@@ -18,10 +21,21 @@ if(file_exists($pidFile)){
 
 define("SOOLEWEB",1);
 error_reporting(0);
+// 创建一个共享内存表
+$globalTable = new Swoole\Table(10);
+$globalTable->column('time', Swoole\Table::TYPE_INT);
+$globalTable->create();
+
 $server = new Swoole\Http\Server("0.0.0.0", $port);
-$server->set(['max_request'=>10000,'pid_file'=>$pidFile, 'enable_coroutine' => false]);
-$server->on('request', function ($request, $response) {
-    
+$conf=array(
+    'max_request'=>10000,
+    'pid_file'=>$pidFile,
+    'dispatch_mode'=>3, 
+    'task_worker_num' =>2,//这个只能一个进程否则.. 
+    'enable_coroutine' => false,
+);
+$server->set($conf);
+$server->on('request', function ($request, $response)  use ($server,$globalTable) {
     //注册异常返回
     register_shutdown_function(function () use ($response) {
         shutdownCall($response);
@@ -32,78 +46,73 @@ $server->on('request', function ($request, $response) {
     $_POST=$request->post;
     $_SERVER['REMOTE_ADDR']=$request->server['remote_addr'];
     $_FILES=$request->files;
+    if(!$request->files){
+        $GLOBALS['raw_content'] = $request->rawContent();
+    }
     $file=$request->server['request_uri'];
     $_REQUEST=array_merge((array)$_GET,(array)$_POST);
-    //runkit和uopz扩展二选一
-    if(function_exists('runkit7_constant_redefine')){
-        defined('NOW')  && runkit7_constant_redefine('NOW', time());
-        defined('SGSID')  &&  runkit7_constant_redefine('SGSID',$_REQUEST['sid']?$_REQUEST['sid']:0);
-        defined('SGLID')  &&  runkit7_constant_redefine('SGLID',$_REQUEST['lid']?$_REQUEST['lid']:0);
-    }elseif(function_exists('uopz_redefine')){
-        defined('NOW')  && uopz_redefine('NOW', time());
-        defined('SGSID')  &&  uopz_redefine('SGSID',$_REQUEST['sid']?$_REQUEST['sid']:0);
-        defined('SGLID')  &&  uopz_redefine('SGLID',$_REQUEST['lid']?$_REQUEST['lid']:0);
-    }
-	
-	$filename = str_replace(basename(__FILE__),'',__FILE__) .'public/'.$file;
-	$ext = pathinfo($filename, PATHINFO_EXTENSION);
-	   
-	$mime_types=array('jpg'=>'image/jpeg','jpeg'=>'image/jpeg',
-	      'png'=>'image/png','gif'=>'image/gif',
-	      'html'=>'text/html','css'=>'text/css',
-	      'js'=>'application/javascript'
-	    );
-     //处理文件
-    if (in_array($ext, ["html", "css", "js", "jpg", "png", "gif",'jpeg'])) {
-	//有相对路径处理(不然不安全)
-	if (strpos($file, "../") !== false || strpos($file, "/") !== 0) {
-	    $response->status(403);
-	    $response->end();
-	}else{
-	    $response->header("Content-Type",$mime_types[$ext]);
-	    $response->sendfile($filename);
-	}
-    }else{
-      
-	    ob_start();
-	    try {
-		include "index.php";//这个是你的框架入口文件
-		$response->end(ob_get_clean());
-	    } catch (Swoole\ExitException $e) {
-		$res=ob_get_clean();
-		if(!$res){
-		    $res=$e->getStatus();
-		}
-		$response->end($res);
-	    }catch (Throwable  $e) {
-		 ob_end_clean();   
-		//写错误日志
-		$response->status(500,"Server Error");
-		$response->end("Server Error");
-	    }
+    $startTime=microtime(true);
+    //保存请求时间
+    $globalTable->set($server->worker_pid, [
+        'time' => $startTime,
+    ]);
+    //载入你的入口文件
+    include "index.php";
+    //更新请求时间
+    $globalTable->set($server->worker_pid, [
+        'time' => 0,
+    ]);
+    //慢日志(大于300毫秒的)
+    $runtime=microtime(true) - $startTime;
+    if($runtime*1000>300){
+        $server->task(json_encode(['url'=>$request->server['request_uri'],'data'=>$_REQUEST, 'runtime'=>$runtime]));
     }
 });
 
-$server->on('finish', function () {
-    echo "task finish\r\n";
-});
+
 
 $server->on("workerError", function (Swoole\Server $server, int $worker_id, int $worker_pid, int $exit_code, int $signal) {
     echo "WorkerError: Worker {$worker_id} error with exit code {$exit_code}, signal {$signal}\n";
 });
 
+// 监听Task事件
+$server->on('Task', function ($http, $task_id, $worker_id, $dataStr) use ($server) {
+    $file = SWROOT.'data/phperror/swRequest.php';
+    $data=json_decode($dataStr,true);
+    $line=date('Y-m-d H:i:s')." slow url:".$data['url'].' data:'.json_encode($data['data']).' runtime:'.number_format($data['runtime']*1000, 3)."ms\r\n";
+    @file_put_contents($file, $line, FILE_APPEND | LOCK_EX);
+    
+});
 //热重启检测
-swoole_timer_tick(1000*45, function () use ($server) {
+$server->tick(1000*30, function () use ($server,$globalTable) {
     if (checkChange()) {
         // 触发重启
         $server->reload();
     }
+    //检测空闲数
+    $stats = $server->stats();
+    if($stats['idle_worker_num']<1){
+        $file = SWROOT.'data/phperror/swRequest.php';
+        $line=date("Y-m-d h:i:s")." not free worker \r\n";
+        @file_put_contents($file, $line, FILE_APPEND | LOCK_EX);
+    }
+    //检测卡死
+    $pids=array();
+    foreach($globalTable as $pid=>$row)
+    {
+        if($row['time']>0&&(microtime(true) - $row['time'])*1000>1000*30){
+            echo "stop pid:{$pid}\r\n";
+            posix_kill($pid, SIGKILL);
+            $pids[]=$pid;
+        } 
+    }
+    //删除key
+    foreach($pids as $pid){
+        $globalTable->del($pid);
+    }
 });
+
 $server->start();
-
-
-
-
 //错误返回500
 function shutdownCall($response){
     $response->status(500,"Server Error");
@@ -119,7 +128,8 @@ function shutdownCall($response){
 	}
 }
 
-//检测文件改变(当然inotify性能更好)
+
+//检测文件改变
 function checkChange(){
     global $fileInfo;
     //监控的目录
@@ -151,5 +161,11 @@ function checkChange(){
     $fileInfo=$newFileInfo;
     return false;
 }
-
-
+function exceptionLog(Throwable $e){
+    $file = SWROOT.'data/phperror/swphperr.php';
+    $error = date('Y-m-d H:i:s') . '---';
+    $error .= 'Error:' . $e->getMessage() . '--';
+    $error .= 'File:' . $e->getFile() . '--';
+    $error .= 'Line:' . $e->getLine();
+    @file_put_contents($file, $error . " \n ", FILE_APPEND | LOCK_EX);
+}
