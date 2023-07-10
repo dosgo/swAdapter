b34d3747e04a0163b6f9e0b99376ddf468f5518c
dosgo <dosgo@qq.com>
2023-07-10 23:15:56 +0800

    Update workServer.php

--- workServer.php
+++ workServer.php
@@ -2,7 +2,6 @@
 ini_set('opcache.enable',0);
 ini_set('opcache.enable_cli',0);
 define("SWROOT",str_replace(basename(__FILE__),'',__FILE__).'../../');
-
 include SWROOT. 'lib/workerman/Autoloader.php';
 use \Workerman\Worker;
 use \Workerman\Protocols\Http\Request;
@@ -10,9 +9,9 @@ use \Workerman\Protocols\Http\Response;
 use \Workerman\Protocols\Http\RawResponse;
 use \Workerman\Protocols\Http\ResponseHeader;
 use \Workerman\Protocols\Http\ResponseCookie;
-/*
-* * * * * /usr/local/php/bing/php workServer.php
-*/
+use \Workerman\Connection\TcpConnection;
+
+
 $pidFile=sys_get_temp_dir().'/'.basename(__FILE__).'.pid';
 $port=isset($argv[2])?intval($argv[2]):9502;
 if(file_exists($pidFile)){
@@ -22,26 +21,27 @@ if(file_exists($pidFile)){
     } 
 }
 
+
 //define("SOOLEWEB",1);
 
 error_reporting(0);
 
 $server = new Worker("http://0.0.0.0:{$port}");
-$server->name = 'MyWebsocketServer';
-$server->count = 4;
+$server->name = 'workmanServer';
+$server->count =4;
 $worker->reusePort = true;
 $server->reloadable = true;
 $server->pidFile = $pidFile;
 //使用swoole(为啥使用,因为它可以捕获exit函数)
 Worker::$eventLoopClass = 'Workerman\Events\Swoole';
+TcpConnection::$defaultMaxPackageSize = 8*1024*1024;
 
-//也使用swoole,我没找到更好用的
-$globalTable = new Swoole\Table(256);
+//也使用swoole,我还找到更好用的
+$globalTable = new Swoole\Table(128);
 $globalTable->column('time', Swoole\Table::TYPE_INT);
 $globalTable->create();
 
-
-$server->onMessage = function ($connection, $request)  use ($server,$globalTable) {
+$server->onMessage = function ($connection, $request)  use ($server,$globalTable,$demo) {
     static $request_count;
     // 处理 GET POST REQUEST $_FILES
     $_GET = $request->get();
@@ -50,6 +50,7 @@ $server->onMessage = function ($connection, $request)  use ($server,$globalTable
     $file = $request->path();
     $_SERVER['REMOTE_ADDR']=$connection->getRemoteIp();
     $_REQUEST=array_merge((array)$_GET,(array)$_POST);
+    $_REQUEST['demo'] =  $demo; //内网
     if(!$request->file()){
         $GLOBALS['raw_content'] = $request->rawBody();
     }
@@ -59,8 +60,7 @@ $server->onMessage = function ($connection, $request)  use ($server,$globalTable
     $globalTable->set(posix_getpid(), [
         'time' => $startTime,
     ]);
-	//include "index.php";
-
+    include "index.php";
     //更新请求时间
     $globalTable->del(posix_getpid());
     $connection->send($response);
@@ -72,21 +72,18 @@ $server->onMessage = function ($connection, $request)  use ($server,$globalTable
 };
 
 // 热重启检测
-$server->onWorkerStart = function ($worker) {
-    $worker->timer_id = \Workerman\Lib\Timer::add(45, function () use ($worker) {
-        if (checkChange()) {
-            echo 'reload'."\r\n";
-            posix_kill(posix_getppid(), SIGUSR1);
-        }
-    });
-};
-
- \Workerman\Lib\Timer::add(10, function () use ($globalTable) {
+\Workerman\Lib\Timer::add(20, function () use ($worker) {
+    if (checkChange()) {
+        echo 'reload'."\r\n";
+        posix_kill(posix_getpid(), SIGUSR1);
+    }
+});
+ \Workerman\Lib\Timer::add(60, function () use ($globalTable) {
    //检测卡死
    $pids=array();
    foreach($globalTable as $pid=>$row)
    {
-       if($row['time']>0&&(microtime(true) - $row['time'])*1000>1000*30){
+       if($row['time']>0&&(microtime(true) - $row['time'])*1000*1000>300){
            echo "stop pid:{$pid}\r\n";
            posix_kill($pid, SIGKILL);
            $pids[]=$pid;
@@ -97,11 +94,5 @@ $server->onWorkerStart = function ($worker) {
        $globalTable->del($pid);
    }
 });
-
-
-
-$server->onWorkerStop = function ($worker) {
-    \Workerman\Lib\Timer::del($worker->timer_id);
-};
 
 Worker::runAll();
