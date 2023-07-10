ec26906f842d4397d57ab52de828092a4a5c5cee
dosgo <dosgo@qq.com>
2023-07-10 23:22:32 +0800

    Update swServer.php

--- swServer.php
+++ swServer.php
@@ -19,36 +19,6 @@ if(file_exists($pidFile)){
     } 
 }
 
-
-$lastTime=time();
-//检测文件改变
-function checkChange(){
-    global $lastTime;
-    //监控的目录
-    $dirs=array(SWROOT.'model',
-                SWROOT.'api',
-                SWROOT.'config',
-                SWROOT.'lib');
-    clearstatcache(true);
-    foreach($dirs as $dir){
-        $reDir = new RecursiveDirectoryIterator($dir);
-        $iterator = new RecursiveIteratorIterator($reDir, RecursiveIteratorIterator::SELF_FIRST);
-        foreach ($iterator as $file) {
-            if ($file->isFile()) {
-                if(pathinfo($file, PATHINFO_EXTENSION) != 'php')
-                {
-                    continue;
-                }
-                if($file->getMTime()>$lastTime){
-                    $lastTime=$file->getMTime();
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
-}
-
 error_reporting(0);
 // 创建一个共享内存表
 $globalTable = new Swoole\Table(128);
