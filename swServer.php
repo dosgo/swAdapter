fefade4f0ef8ea974470db141d8d3975f2e04338
dosgo <dosgo@qq.com>
2023-07-10 23:43:39 +0800

    Update swServer.php

--- swServer.php
+++ swServer.php
@@ -1,5 +1,5 @@
 <?php
-define("SWROOT",str_replace(basename(__FILE__),'',__FILE__).'../../');
+define("SWROOT",str_replace(basename(__FILE__),'',__FILE__));
 include "inc.php";
 /*
 *注意禁止开启协程功能,如果开启不得使用超全局变量,全局变量,静态类,单例数据库模式
