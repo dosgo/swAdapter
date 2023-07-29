084afc4c7fb2bba796cf66a2ef35dac2fb214b87
dosgo <dosgo@qq.com>
2023-07-29 11:46:00 +0800

    Update swServer.php

--- swServer.php
+++ swServer.php
@@ -1,5 +1,7 @@
 <?php
-define("SWROOT",str_replace(basename(__FILE__),'',__FILE__));
+define("SOOLEWEB",1);
+error_reporting(0);
+define("SWROOT",str_replace(basename(__FILE__),'',__FILE__).'../../');
 include "inc.php";
 /*
 *注意禁止开启协程功能,如果开启不得使用超全局变量,全局变量,静态类,单例数据库模式
@@ -11,11 +13,10 @@ if (version_compare(phpversion('swoole'), '4.6.2', '<')) {
 }
 $pidFile=sys_get_temp_dir().'/'.basename(__FILE__).'.pid';
 $port=isset($argv[1])?intval($argv[1]):9501;
-if(checkRun($pidFile)){
-   return false;	
+$demo=isset($argv[2])?intval($argv[2]):0;
+if(checkRun($pidFile,$port)){
+    return false;
 }
-
-error_reporting(0);
 // 创建一个共享内存表
 $globalTable = new Swoole\Table(128);
 $globalTable->column('time', Swoole\Table::TYPE_INT);
@@ -26,7 +27,8 @@ $conf=array(
     'max_request'=>10000,
     'pid_file'=>$pidFile,
     'dispatch_mode'=>3, 
-    'worker_num'=>$demo?4:100,
+    'task_max_request'=>10000,
+    'worker_num'=>4,
     'package_max_length'=> 1024*1024*8,//8M
     'task_worker_num' =>2,
     'enable_coroutine' => false,
@@ -34,12 +36,6 @@ $conf=array(
 );
 $server->set($conf);
 $server->on('request', function ($request, $response)  use ($server,$globalTable,$demo) {
-    $requestData=array_merge((array)$request->get,(array)$request->post);
-    //注册异常返回
-    register_shutdown_function(function () use ($response, $requestData,$request) {
-        shutdownCall($response, $requestData,$request->server['request_uri']);
-    });
-
     //处理GET POST REQUEST $_FILES
     $_GET=$request->get;
     $_POST=$request->post;
@@ -55,13 +51,13 @@ $server->on('request', function ($request, $response)  use ($server,$globalTable
     $globalTable->set($server->worker_pid, [
         'time' => $startTime,
     ]);
-    include "index.php";
+    include "index.php";//
     //删除请求时间
     $globalTable->del($server->worker_pid);
     //慢日志(大于300毫秒的)
     $runtime=microtime(true) - $startTime;
     if($runtime*1000>300){
-        $server->task(json_encode(['url'=>$request->server['request_uri'],'data'=>$_REQUEST, 'runtime'=>$runtime]));
+        $server->task(['url'=>$request->server['request_uri'],'data'=>$_REQUEST, 'runtime'=>$runtime]);
     }
 });
 
@@ -70,26 +66,11 @@ $server->on("workerError", function (Swoole\Server $server, int $worker_id, int 
 });
 
 // 监听Task事件
-$server->on('Task', function ($http, $task_id, $worker_id, $dataStr) use ($server) {
+$server->on('Task', function ($http, $task_id, $worker_id, $data) use ($server) {
     $file = SWROOT.'data/phperror/swRequest.php';
-    $data=json_decode($dataStr,true);
     $line=date('Y-m-d H:i:s')." slow url:".$data['url'].' data:'.json_encode($data['data']).' runtime:'.number_format($data['runtime']*1000, 3)."ms\r\n";
     @file_put_contents($file, $line, FILE_APPEND | LOCK_EX);
     
-});
-//热重启检测
-$server->tick(1000*20, function () use ($server) {
-    if (checkChange()) {
-        // 触发重启
-        $server->reload();
-    }
-    //检测空闲数
-    $stats = $server->stats();
-    if($stats['idle_worker_num']<1){
-        $file = SWROOT.'data/phperror/swRequest.php';
-        $line=date("Y-m-d h:i:s")." not free worker \r\n";
-        @file_put_contents($file, $line, FILE_APPEND | LOCK_EX);
-    }
 });
 
 //卡死检测
@@ -99,7 +80,6 @@ $server->tick(1000*60, function () use ($server,$globalTable) {
     foreach($globalTable as $pid=>$row)
     {
         if($row['time']>0&&(microtime(true) - $row['time'])*1000*1000>300){
-            echo "stop pid:{$pid}\r\n";
             posix_kill($pid, SIGKILL);
             $pids[]=$pid;
         }
@@ -107,6 +87,24 @@ $server->tick(1000*60, function () use ($server,$globalTable) {
     //删除key
     foreach($pids as $pid){
         $globalTable->del($pid);
+    }
+});
+
+
+//在worker进程执行的
+$server->on('WorkerStart', function ($server, $worker_id)  use($pidFile){
+    //task进程来触发
+    if ($worker_id == $server->setting['worker_num']&&$server->taskworker) {
+        file_put_contents($pidFile.'.manager', $server->manager_pid);
+        $GLOBALS['lastTime']=time();
+        //热重启检测
+        $server->tick(1000*10, function () use ($server) {
+            if (checkChange()) {
+                // 触发重启
+               // $server->reload();
+               posix_kill(posix_getppid(), SIGUSR1);
+            }
+        });
     }
 });
 $server->start();
