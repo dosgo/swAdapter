21e91b3962e3dc6a21e9a4f2c92145e1abe15c08
dosgo <dosgo@qq.com>
2023-07-29 11:42:58 +0800

    Update inc.php

--- inc.php
+++ inc.php
@@ -1,8 +1,7 @@
 <?php
-$lastTime=time();
+$GLOBALS['lastTime']=time();
 //检测文件改变
 function checkChange(){
-    global $lastTime;
     //监控的目录
     $dirs=array(SWROOT.'model',
                 SWROOT.'api',
@@ -18,8 +17,8 @@ function checkChange(){
                 {
                     continue;
                 }
-                if($file->getMTime()>$lastTime){
-                    $lastTime=time();
+                if($file->getMTime()>$GLOBALS['lastTime']){
+                    $GLOBALS['lastTime']=time();
                     return true;
                 }
             }
@@ -28,12 +27,41 @@ function checkChange(){
     return false;
 }
 
-function checkRun($pidFile){
+function checkRun($pidFile,$port){
+    $client = new Swoole\Client(SWOOLE_SOCK_TCP);
+    $run=true;
+    if (!$client->connect('127.0.0.1', intval($port), 0.5)) {
+        $run=false;
+    } 
+    $client->close();
     if(file_exists($pidFile)){
         $pid = intval(file_get_contents($pidFile));
-        if (posix_kill($pid, SIG_DFL)) {//判断该进程是否正常运行中
-            return $pid;
+        $cmdline=file_get_contents("/proc/{$pid}/cmdline");
+        //表示僵尸进程(崩溃了)
+        if(strlen($cmdline)<3||!posix_kill($pid, SIG_DFL)){
+            $run=false;
         }
+        //如果是swoole manager进程崩溃也不行
+        if(defined('SOOLEWEB')){
+            $managerPid = intval(file_get_contents($pidFile.'.manager'));
+            $cmdline=file_get_contents("/proc/{$managerPid}/cmdline");
+            //表示僵尸进程(崩溃了)
+            if(strlen($cmdline)<3||!posix_kill($managerPid, SIG_DFL)){
+                $run=false;
+            }
+        }
+        if(!$run){
+            //结束子进程
+            $pgid=posix_getpgid($pid);
+            if($pgid>0){
+                posix_kill(-$pgid,SIGKILL);
+            }else{
+                posix_kill(-$pid,SIGKILL);
+            }
+            usleep(5000);
+        }
+    }else{
+        $run=false;
     }
-    return false;
+    return $run;
 }
